0fb07d501dd2f65d52e1e34f116c1cfeeaeb717e
Thiago Mascarenhas Oliveira <thiago_si@ymail.com>
2014-10-12 17:16:08 -0300

    testando

---
+++ _breadcrumb.php
@@ -0,0 +1,11 @@
+				<!-- Breadcrumb -->
+                <ul class="breadcrumb" data-spy="affix" data-offset-top="250">
+                    <li>
+                        <a href="index.html"><i class="glyphicon-user"></i></a> <span class="divider"><i class="icon-angle-right"></i></span>
+                    </li>
+                    <li>
+                        <a href="#">Usuarios</a> <span class="divider"><i class="icon-angle-right"></i></span>
+                    </li>
+                    <li class="active"><a href="">Novo</a></li>
+                </ul>
+                <!-- END Breadcrumb -->
